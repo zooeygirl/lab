f527d20b6391ccb7694c140e54cf3d1b8a4956f0
zooeygirl <zooeythecalisi@gmail.com>
2019-08-29 21:40:08 +0200

    fixed teachers api

--- routes/teachers.js
+++ routes/teachers.js
@@ -3,11 +3,10 @@ const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
 const express = require("express");
 const router = express.Router();
-const _ = require("lodash");
 
 router.get("/", async (req, res) => {
   const teachers = await User.find({ role: "Teacher" }).sort("lastname");
-  res.send(_.pick(teachers, ["lastname"]));
+  res.send(teachers);
 });
 
 module.exports = router;
